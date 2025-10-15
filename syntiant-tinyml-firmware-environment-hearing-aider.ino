4b73fda644ba227302aa3259be74104da4fb441f
Solomon Muhunyo Githu <solomgithu@gmail.com>
2025-10-15 21:25:26 +0300

    Fixed vibrating motor control logic

--- syntiant-tinyml-firmware-environment-hearing-aider.ino
+++ syntiant-tinyml-firmware-environment-hearing-aider.ino
@@ -12,21 +12,22 @@
 */
 
 // Updated June 2023, Solomon Muhunyo Githu
+// Updated October 2025, fixed vibrating motor control logic
 void on_classification_changed(const char *event, float confidence, float anomaly_score) {
   if ( (strcmp(event, "car_horn") == 0) || (strcmp(event, "ambulance_firetruck") == 0) ) {
     digitalWrite(LED_RED, HIGH);
-    OUT_1_HIGH(); // set output to HIGH on GPIO 1
+    OUT_1_LOW(); // set output to HIGH on GPIO 1
     //OUT_2_HIGH(); // set output to HIGH on GPIO 2
     //OUT_3_HIGH(); // set output to HIGH on GPIO 3
     delay(1000);
-    OUT_1_LOW(); // set output to LOW on GPIO 1
+    OUT_1_HIGH(); // set output to LOW on GPIO 1
     //OUT_2_LOW(); // set output to LOW on GPIO 2
     //OUT_3_LOW(); // set output to LOW on GPIO 3
   }
 
   else {
     digitalWrite(LED_RED, LOW);
-    OUT_1_LOW(); // set output to LOW on GPIO 1
+    OUT_1_HIGH(); // set output to LOW on GPIO 1
     //OUT_2_LOW(); // set output to LOW on GPIO 2
     //OUT_3_LOW(); // set output to LOW on GPIO 3
   }
